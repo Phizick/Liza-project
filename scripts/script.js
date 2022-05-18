2b4130020e22a2ba121d1cedcd860fdf1e75f656
Nick White <bbycinka@gmail.com>
2022-05-18 09:32:50 +0300

    bufix: добавление transition и новое проверочное условие

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 const slideBarItem = document.querySelectorAll(".slidebar__list-item");
 slideBarItem.forEach(function (i) {
   i.addEventListener("click", function () {
-    i.children[1] ? i.children[1].classList.toggle("items__list_active") : null; // получаем элемент со списком
-    i.children[0].children[1].classList.toggle("list-item__icon_active"); // получаю элемент ^ или v
+    if (i.children[1].classList.contains("items__list")) {
+      i.children[1].classList.toggle("items__list_active");
+      i.children[0].children[1].classList.toggle("list-item__icon_active");
+    }
   });
 });
