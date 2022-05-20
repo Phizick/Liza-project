c722a9993647419e7864c4c21d46b237ae00f9d7
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2022-05-20 19:18:51 +0300

    added js-test

---
+++ script2.js
@@ -0,0 +1,41 @@
+const formBtn = document.querySelector(".button__result");
+
+
+
+const testMarker = document.getElementsByName('question');
+
+
+testMarker.forEach(item => {
+item.addEventListener('click', () => {
+    if ( item.checked === true) {
+        formBtn.classList.add("button__result_active");
+    } if (item.checked === false) {
+        formBtn.classList.remove("button__result_active")
+    }
+})})
+
+let inp = document.getElementsByName('question')
+
+// function check() {
+//     for (i = 0; i < inp.length; i++) {
+//         if (inp[i].type == "radio" && inp[i].checked) {
+//             a = true;
+//         }
+//     }
+//     return a
+// }
+
+
+
+function countTest() {
+    if (formBtn.classList.contains("button__result_active") 
+    && ((questionTwo.checked & questionThree.checked) 
+    || (questionTwo.checked & questionOne.checked) 
+    || (questionOne.checked & questionThree.checked)
+    || (questionOne.checked & questionTwo.checked)
+    && inp.checked)) {
+    window.location.href = 'testPassed.html';
+    }
+    
+}
+
