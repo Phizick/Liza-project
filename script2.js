d08ca5acf79229f84abf167c52043759e2ea0595
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2022-05-22 17:45:02 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,8 +1,9 @@
 const formBtn = document.querySelector(".button__result");
 
 
-
 const testMarker = document.getElementsByName('question');
+const testMarkerCheck = document.getElementsByName("question__first");
+
 
 
 testMarker.forEach(item => {
@@ -14,27 +15,37 @@ item.addEventListener('click', () => {
     }
 })})
 
-let inp = document.getElementsByName('question')
+testMarkerCheck.forEach(item => {
+    item.addEventListener('click', () => {
+        if ( item.checked === true) {
+            formBtn.classList.add("button__result_active");
+        } if (item.checked === false) {
+            formBtn.classList.remove("button__result_active")
+        }
+})})
 
-// function check() {
-//     for (i = 0; i < inp.length; i++) {
-//         if (inp[i].type == "radio" && inp[i].checked) {
-//             a = true;
-//         }
-//     }
-//     return a
-// }
 
+
+function resulted() {
+    let count = 0;
+for (i = 0; i < testMarkerCheck.length; i++) {
+    let check = testMarkerCheck[i];    
+    if (check.checked === true) {
+        count++
+    }    
+}
+return count;
+}
+ 
 
 
 function countTest() {
     if (formBtn.classList.contains("button__result_active") 
-    && ((questionTwo.checked & questionThree.checked) 
-    || (questionTwo.checked & questionOne.checked) 
-    || (questionOne.checked & questionThree.checked)
-    || (questionOne.checked & questionTwo.checked)
-    && inp.checked)) {
+    && (resulted() >= 1)
+    & questionFive.checked) {
     this.location.href = 'testPassed.html';
+    } else {
+        this.location.href = 'testFailed.html'
     }
     
 }
