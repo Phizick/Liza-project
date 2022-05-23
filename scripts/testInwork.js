2fc979a7949b8faf669329c914caba0fb7690624
Stepan Burlakov <burlackoff@gmail.com>
2022-05-23 23:46:53 +0300

    feat: add testInWork.html + fixbugs

--- scripts/testInwork.js
+++ scripts/testInwork.js
@@ -7,24 +7,25 @@ const testMarkerCheck = document.getElementsByName("question__first");
 
 function resulted() {
     let count = 0;
-for (i = 0; i < testMarkerCheck.length; i++) {
-    let check = testMarkerCheck[i];    
-    if (check.checked === true) {
-        count++
-    }    
-}
-return count;
+    for (i = 0; i < testMarkerCheck.length; i++) {
+        let check = testMarkerCheck[i];
+        if (check.checked === true) {
+            count++
+        }
+    }
+    return count;
 }
 let radioValueCount = 0
 testMarker.forEach(item => {
-this.addEventListener('click', () => {
-    if ( item.checked === true) {
-        formBtn.classList.add("button__result_active");
-        radioValueCount++;
-    } if (resulted() <= 0) {
-        formBtn.classList.remove("button__result_active")
-    }
-})})
+    this.addEventListener('click', () => {
+        if (item.checked === true) {
+            formBtn.classList.add("button__result_active");
+            radioValueCount++;
+        } if (resulted() <= 0) {
+            formBtn.classList.remove("button__result_active")
+        }
+    })
+})
 
 testMarkerCheck.forEach(item => {
     this.addEventListener('click', () => {
@@ -33,16 +34,17 @@ testMarkerCheck.forEach(item => {
         } if (resulted() <= 0) {
             formBtn.classList.remove("button__result_active")
         }
-})})
+    })
+})
 
 
 function countTest() {
-    if (formBtn.classList.contains("button__result_active") 
-    && (resulted() >= 2)
-    & questionFive.checked) {
-    this.location.href = 'testPassed.html';
+    if (formBtn.classList.contains("button__result_active")
+        && (resulted() >= 2)
+        & questionFive.checked) {
+        this.location.href = 'testPassed.html';
     } else {
         this.location.href = 'testFailed.html'
-    }    
+    }
 }
 
